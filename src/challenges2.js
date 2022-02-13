72a3bee7bf1fbb468a73a46b389579460a09502d
Murilo <murilocarrijoadm@gmail.com>
2022-02-13 11:35:37 -0300

    Crie uma função de Lista de Tecnologias

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,6 +1,21 @@
-// Desafio 10
-function techList() {
-  // seu código aqui
+// Desafio 10 - Crie uma função que receba um array de Lista de Tecnologias e retorne as tecnologias ordenadas alfabeticamente
+// e com o nome da pessoa que deseja aprende-la
+function techList(techs, name) {
+  const arrObj = [];
+  let orderTechs;
+  for (let i = 0; i <= techs.length; i += 1) {
+    orderTechs = techs.sort();
+    if (techs.length > 0) {
+      arrObj.push({
+        tech: orderTechs[i],
+        name,
+      });
+    } else if (techs) {
+      return 'Vazio!';
+    }
+  }
+  arrObj.pop();
+  return arrObj;
 }
 
 // Desafio 11
